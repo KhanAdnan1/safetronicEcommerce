c4119c7f751a6e39d364826ad4e588734de386e5
KhanAdnan1 <ka4180910@gmail.com>
2025-03-17 01:30:08 +0530

    API Changes

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -50,7 +50,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "http://localhost:8080/api/admin/products/upload-image",
+      "https://safetronicecommerceserver.onrender.com/api/admin/products/upload-image",
       data
     );
     console.log(response, "response");
--- client/src/store/shop/search-slice/index.js
+++ client/src/store/shop/search-slice/index.js
@@ -10,7 +10,7 @@ export const getSearchResults = createAsyncThunk(
   "/order/getSearchResults",
   async (keyword) => {
     const response = await axios.get(
-      `http://localhost:8080/api/shop/search/${keyword}`
+      `https://safetronicecommerceserver.onrender.com/api/shop/search/${keyword}`
     );
 
     return response.data;
