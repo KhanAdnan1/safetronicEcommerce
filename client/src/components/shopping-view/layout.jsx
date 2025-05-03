a30645cd8e19e87e40e98c3186036069db4bd8c7
KhanAdnan1 <ka4180910@gmail.com>
2025-05-03 18:41:39 +0530

    footer

--- client/src/components/shopping-view/layout.jsx
+++ client/src/components/shopping-view/layout.jsx
@@ -32,6 +32,12 @@ function ShoppingLayout() {
 
       {/* Footer */}
       <footer className="text-center text-sm text-white py-4 bg-black">
+      <div className="mb-2 space-x-6">
+    <a href="https://safetronic.onrender.com/" className="hover:underline text-gray-300" target="_blank" rel="noopener noreferrer">Blog</a>
+    <a href="https://safetronic.onrender.com/about" className="hover:underline text-gray-300" target="_blank" rel="noopener noreferrer">About</a>
+    <a href="https://safetronic.onrender.com/contact" className="hover:underline text-gray-300" target="_blank" rel="noopener noreferrer">Contact Us</a>
+    <a href="https://safetronic.onrender.com/services" className="hover:underline text-gray-300" target="_blank" rel="noopener noreferrer">Our Services</a>
+  </div>
         &copy; 2024 Safetronic. All rights reserved 
       </footer>
     </div>
